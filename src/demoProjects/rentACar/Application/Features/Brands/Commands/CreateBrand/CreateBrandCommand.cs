fbd6d9305dd4535dd458e0457f1c723e7bbaa554
Onurtaci <onurtaci1819@gmail.com>
2024-02-23 19:27:35 +0300

    Senior Level Camp Day 3

--- src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Commands.CreateBrand
 {
-    public partial class CreateBrandCommand : IRequest<CreatedBrandDto>
+    public class CreateBrandCommand : IRequest<CreatedBrandDto>
     {
         public string Name { get; set; }
 
